f247e436fdd708a92e5b308c43439cd6aa43a55e
Dario Breitenstein <dario@imakethings.ch>
2023-01-03 23:55:37 +0100

    remove unnecessary mkdir from generate script

--- scripts/generate_grpc.sh
+++ scripts/generate_grpc.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-mkdir ./photobox_backend_grpc/grpc
 python -m grpc_tools.protoc -I ./protos --python_out=./photobox_backend_grpc --pyi_out=./photobox_backend_grpc --grpc_python_out=./photobox_backend_grpc ./protos/photobox_service.proto
